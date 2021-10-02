b60c00b05d46a783796a5e39f9315bbd378c5d9a
Yuriy <Seaman_on_watch@mail.ru>
2021-10-02 20:26:51 +0300

    Create test

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -1,9 +1,26 @@
-describe("Test Contact Us form via WebDriverUni", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
+/// <reference types="cypress" />
 
-    })
+describe("Test Contact Us form via WebdriverUni", () => {
+    it.only("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        //cy.get('#contact-us').click({force: true})
+        cy.contains('CONTACT US')
+        cy.get('#nav-title').contains("WebdriverUniversity.com (New Approach To Learning)")
+        cy.get('[type="reset"]').should('have.value', 'RESET')
+        cy.get('[type="submit"]').should('have.value', 'SUBMIT')
+        cy.get('[name="first_name"]').type("Joe");
+        cy.get('[name="last_name"]').type("blogs");
+        cy.get('[name="email"]').type("joe_blogs123@gmail.com")
+        cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+        cy.get('[type="submit"]').click();
+        cy.contains('Thank You for your Message!')
+    });
 
-    it("Should not be able to submit a successful submission via contact us form", () => {
-
-    })
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.get('[name="first_name"]').type("Tom");
+        cy.get('[name="last_name"]').type("blogs");
+        cy.get('textarea.feedback-input').type("How can I learn Cypress?")
+        cy.get('[type="submit"]').click();
+    });
 })
