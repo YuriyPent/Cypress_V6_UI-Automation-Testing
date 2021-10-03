5ad32964e0a4980f3bff9048396ad2ca30c2363e
Yuriy <Seaman_on_watch@mail.ru>
2021-10-03 16:32:35 +0300

    Improving Tests with Dynamic Selectors

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -8,21 +8,34 @@ describe("Test Contact Us form via Automation Test Store", () => {
             , body: '{"cart_details":"\t<div class=\"empty_cart text-center\">\n\t\t<i class=\"fa fa-shopping-cart\"><\/i>\n\t<\/div>\n","item_count":1,"total":"$1.00"}'
         }).as('addToCart');
 
+        cy.intercept('POST', '**/visit_url/', {
+            statusCode: 200
+            , body: '{"msg": "success", "is_error": false, "code": 200}'
+        }).as('visit_url');
+
         cy.intercept('GET', '**/banner_manager*', {
             statusCode: 200,
             body: '{ "success": "OK" }'
         }).as('banner_manager');
 
-        cy.intercept('GET', '/users/get_id*', {
-            statusCode: 200
-            , body: '{"code": 200, "is_error": false, "data": "OK"}'
-        }).as('get_id');
+        cy.intercept('POST', '**/send_user_data/', {
+            statusCode: 200,
+            body: '{"msg": "success", "is_error": false, "code": 200}'
+        }).as('send_user_data');
+
+        // cy.intercept('GET', '/users/get_id*', {
+        //     statusCode: 200
+        //     , body: '{"code": 200, "is_error": false, "data": "OK"}'
+        // }).as('get_id');
 
         cy.visit("https://www.automationteststore.com/");
-        cy.xpath('(//*[text()="Contact Us"])[2]').click();
-        cy.get('[id="ContactUsFrm_first_name"]').type("Joe");
-        cy.get('[id="ContactUsFrm_email"]').type("joe_blog01@gmail.com");
-        cy.get('[id="ContactUsFrm_enquiry"]').type("Do you provide additional on bulk orders?");
-        cy.get('button[title="Submit"]').click();
+        cy.get("a[href$='contact']").click();
+        cy.xpath('//h2[starts-with(text(), "Con")]/following-sibling::ul/li[1]').contains("+123 456 7890");
+        cy.xpath('//h2[starts-with(text(), "Con")]/following-sibling::ul/li[2]').contains("admin@automationteststore.com");
+        cy.get('#ContactUsFrm_first_name').type("Joe");
+        cy.get('#ContactUsFrm_email').type("joe_blog01@gmail.com");
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide additional on bulk orders?");
+        cy.get("button[title='Submit']").click();
+        cy.get('.mb40 > :nth-child(3)').contains("Your enquiry has been successfully sent to the store owner!")
     })
 })
